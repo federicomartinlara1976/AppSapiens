8fb170f254754467a1680d60c530db995783e0c2
Federico Martín Lara <federico@horizontefinal>
2021-08-06 10:55:48 +0200

    Remove main

--- AppSapiens/src/main/java/com/sapiens/app/ui/FramePrincipal.java
+++ AppSapiens/src/main/java/com/sapiens/app/ui/FramePrincipal.java
@@ -71,35 +71,4 @@ public class FramePrincipal extends JFrame {
 
 		pack();
 	}// </editor-fold>//GEN-END:initComponents
-
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String args[]) {
-		/* Set the Nimbus look and feel */
-		// <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
-		// (optional) ">
-		/*
-		 * If Nimbus (introduced in Java SE 6) is not available, stay with the default
-		 * look and feel. For details see
-		 * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-		 */
-		try {
-			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				if ("Nimbus".equals(info.getName())) {
-					UIManager.setLookAndFeel(info.getClassName());
-					break;
-				}
-			}
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException ex) {
-			log.error("FATAL:", ex);
-		}
-		// </editor-fold>
-
-		/* Create and display the form */
-		EventQueue.invokeLater(() -> {
-			new FramePrincipal().setVisible(true);
-		});
-	}
 }
