60c8a2a127daf59f5b85b628d2f86cd43d7f8fb1
hcarreno <hcarreno.pex@sanitas.es>
2021-08-10 14:59:41 +0200

    configuration singleton

---
+++ AppSapiens/src/main/java/com/sapiens/app/utils/ConfigurationSingleton.java
@@ -0,0 +1,46 @@
+package com.sapiens.app.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author hcarreno
+ *
+ */
+public class ConfigurationSingleton {
+
+	private static ConfigurationSingleton instance;
+
+	private Properties properties;
+
+	private ConfigurationSingleton() throws IOException {
+		properties = new Properties();
+		
+		//the base folder is ./, the root of the main.properties file  
+		String userDir = System.getProperty("user.dir");
+		String literalesPropertiesPath = userDir + "/AppSapiens/configuration.properties";
+		
+		FileInputStream fistream = new FileInputStream(literalesPropertiesPath);
+		properties.load(fistream);
+	}
+	
+	public static ConfigurationSingleton getInstance() throws IOException {
+		if (instance == null) {
+			instance = new ConfigurationSingleton();
+		}
+		return instance;
+	}
+	
+	/**
+	 * @param key
+	 * @return
+	 */
+	public String getConfig(String key) {
+		String value = properties.getProperty(key);
+
+		return StringUtils.isNotBlank(value) ? value : StringUtils.EMPTY;
+	}
+}
