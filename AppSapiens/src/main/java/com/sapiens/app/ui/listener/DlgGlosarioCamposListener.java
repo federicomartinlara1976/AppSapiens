f6dd722c9ecb0ab897e263fac994de5c8abfaa97
Federico Martín Lara <federico@horizontefinal>
2021-08-12 09:03:38 +0200

    Code fixes

--- AppSapiens/src/main/java/com/sapiens/app/ui/listener/DlgGlosarioCamposListener.java
+++ AppSapiens/src/main/java/com/sapiens/app/ui/listener/DlgGlosarioCamposListener.java
@@ -4,12 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JDialog;
 
-import com.sapiens.app.ui.glosarios.DlgDefinicionGlosarios;
 import com.sapiens.app.ui.glosarios.DlgGlosarioCampos;
-import com.sapiens.app.ui.utils.UIHelper;
-import com.sapiens.app.utils.Constants;
 
 public class DlgGlosarioCamposListener extends ListenerSupport implements ActionListener {
 
