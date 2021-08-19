25f9c8ce18ed85c87b66f30af521561f69e5eac9
Federico Martín Lara <federico@horizontefinal>
2021-08-19 13:37:56 +0200

    DialogSupport

--- AppSapiens/src/main/java/com/sapiens/app/ui/utils/DialogSupport.java
+++ AppSapiens/src/main/java/com/sapiens/app/ui/utils/DialogSupport.java
@@ -1,11 +1,18 @@
 package com.sapiens.app.ui.utils;
 
+import java.io.IOException;
+
 import javax.swing.JDialog;
+
+import com.sapiens.app.utils.LiteralesSingleton;
+
+import lombok.extern.log4j.Log4j;
 
 /**
  * @author federico
  *
  */
+@Log4j
 public abstract class DialogSupport extends JDialog {
 
 	/**
@@ -13,10 +20,43 @@ public abstract class DialogSupport extends JDialog {
 	 */
 	private static final long serialVersionUID = -637526827846474731L;
 
+	protected LiteralesSingleton literales;
+	
 	/**
 	 * 
 	 */
 	public DialogSupport() {
 		super();
+		
+		initialize();
+	}
+	
+	/**
+     * 
+     */
+    private void initialize() {
+		try {
+			initComponents();
+			initLiterals();
+			initEvents();
+			
+			initialState();
+		} catch (IOException e) {
+			log.warn("ERROR:", e);
+		}
+	}
+	
+	protected abstract void initComponents();
+	
+	protected abstract void initEvents();
+	
+	protected abstract void initialState();
+	
+	protected abstract void setupLiterals();
+	
+	private void initLiterals() throws IOException {
+		literales = LiteralesSingleton.getInstance();
+		
+		setupLiterals();
 	}
 }
