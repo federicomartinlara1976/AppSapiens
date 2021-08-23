b02f0c9e30ff3983ff43b70dd294469b3bcae30e
Federico Martín Lara <federico@horizontefinal>
2021-08-23 14:17:16 +0200

    Método de búsqueda

--- AppSapiens/src/main/java/com/mdval/ui/listener/FrmDefinicionGlosariosListener.java
+++ AppSapiens/src/main/java/com/mdval/ui/listener/FrmDefinicionGlosariosListener.java
@@ -28,7 +28,7 @@ public class FrmDefinicionGlosariosListener extends ListenerSupport implements A
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton jButton = (JButton) e.getSource();
-		
+
 		if (Constants.DLG_DEFINICION_GLOSARIOS_BTN_BUSCAR.equals(jButton.getName())) {
 			eventBtnBuscar();
 		}
@@ -46,8 +46,13 @@ public class FrmDefinicionGlosariosListener extends ListenerSupport implements A
 		}
 	}
 
+	/**
+	 * 
+	 */
 	private void eventBtnBuscar() {
-		populateModel();
+		String cadenaBuscar = frmDefinicionGlosarios.getTxtGlosario().getText();
+		List<Glosario> glosarios = buscar(cadenaBuscar);
+		populateModel(glosarios);
 	}
 
 	private void eventBtnAlta() {
@@ -57,7 +62,7 @@ public class FrmDefinicionGlosariosListener extends ListenerSupport implements A
 	private void evntBtnModificacion() {
 		Map<String, Object> params = new HashMap<>();
 		params.put(Constants.DLG_DEFINICION_GLOSARIOS_SELECCIONADO, frmDefinicionGlosarios.getSeleccionado());
-		
+
 		showPopup(frmDefinicionGlosarios, Constants.CMD_MODIFICACION_GLOSARIOS, params);
 	}
 
@@ -65,28 +70,41 @@ public class FrmDefinicionGlosariosListener extends ListenerSupport implements A
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	/**
-	 * TODO - Borrar este método de prueba y usar una conexión a BBDD
+	 * Vuelca la lista de glosarios encontrados en la tabla
 	 * 
 	 * @return
 	 */
-	private void populateModel() {
-		// Test - add Glosario to model
-		List<Glosario> glosarios = new ArrayList<>();
-		
+	private void populateModel(List<Glosario> glosarios) {
+		// Obtiene el modelo y lo actualiza
+		DefinicionGlosariosTableModel tableModel = (DefinicionGlosariosTableModel) frmDefinicionGlosarios
+				.getTblGlosarios().getModel();
+		tableModel.setGlosarios(glosarios);
 
-		// Populate with 100 glosarios
+		// Para que los cambios sean efectivos debe lanzar la notificación. La selección
+		// que hubiera se perderá
+		tableModel.fireTableDataChanged();
+
+		// Como se ha regenerado el contenido de la tabla y se ha perdido la selección,
+		// deshabilitar el botón de selección para la próxima.
+		frmDefinicionGlosarios.getBtnModificacion().setEnabled(Boolean.FALSE);
+	}
+
+	/**
+	 * TODO - Metodo de búsqueda a modificar con la llamada al servicio
+	 * 
+	 * @param termino
+	 * @return
+	 */
+	private List<Glosario> buscar(String termino) {
+		List<Glosario> glosarios = new ArrayList<>();
+
+		// Sample: Populate with 100 objects of type Glosario
 		for (int i = 0; i < 100; i++) {
 			Glosario glosario = new Glosario(i, "Glosario " + i, "usuario", new Date(), new Date());
 			glosarios.add(glosario);
 		}
-
-		DefinicionGlosariosTableModel tableModel = (DefinicionGlosariosTableModel) frmDefinicionGlosarios.getTblGlosarios().getModel();
-		tableModel.setGlosarios(glosarios);
-		frmDefinicionGlosarios.getTblGlosarios().setModel(tableModel);
-		tableModel.fireTableDataChanged();
-		
-		frmDefinicionGlosarios.getBtnModificacion().setEnabled(Boolean.FALSE);
+		return glosarios;
 	}
 }
