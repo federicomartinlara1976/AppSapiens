f4e8cef70db6c7f2387211e67ef5f22095631eb8
Federico Martín Lara <federico@horizontefinal>
2021-08-26 22:07:02 +0200

    format code

--- AppSapiens/src/main/java/com/mdval/ui/PanelLogotipo.java
+++ AppSapiens/src/main/java/com/mdval/ui/PanelLogotipo.java
@@ -19,24 +19,24 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class PanelLogotipo extends JPanel {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3034469653390494133L;
 	private BufferedImage image;
 
-    public PanelLogotipo(String logotipo) {
-       try {                
-          image = ImageIO.read(new File("./" + logotipo));
-       } catch (IOException ex) {
-            LogWrapper.warn(log, "ERROR:", ex);
-       }
-    }
+	public PanelLogotipo(String logotipo) {
+		try {
+			image = ImageIO.read(new File("./" + logotipo));
+		} catch (IOException ex) {
+			LogWrapper.warn(log, "ERROR:", ex);
+		}
+	}
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters            
-    }
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters
+	}
 
 }
